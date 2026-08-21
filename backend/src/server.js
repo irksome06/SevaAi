b7b979abee666fcd866e8dfaa6d9f3ad851de0d7
expense-tracker <kritika161106@gmail.com>
2026-08-21 16:23:56 +0530

    remove the problrm port

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = require('./app');
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+
 
 // Connect to MongoDB and start HTTP Server
 const startServer = async () => {
